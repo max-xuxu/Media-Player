49311ac09969ed0654df542c8db95b1511df871c
maxXu <3167683520@qq.com>
2025-10-20 19:41:16 +0800

    Add LoginDialog class definition

---
+++ LoginDialog.h
@@ -0,0 +1,27 @@
+#ifndef LOGINDIALOG_H
+#define LOGINDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class LoginDialog;
+}
+
+class LoginDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit LoginDialog(QWidget *parent = nullptr);
+    ~LoginDialog();
+    
+private slots:
+    void on_pushButton_login_clicked();
+    
+    void on_pushButton_register_clicked();
+    
+private:
+    Ui::LoginDialog *ui;
+};
+
+#endif // LOGINDIALOG_H
